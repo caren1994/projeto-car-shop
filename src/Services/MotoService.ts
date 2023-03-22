ccba45e15408268679cb73285f4dce621cd2c002
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 18:40:17 -0300

    feat:find e findid moto

--- src/Services/MotoService.ts
+++ src/Services/MotoService.ts
@@ -1,7 +1,8 @@
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotoODM from '../Models/MotoODM';
-import { resCorrect } from '../utils/funcResp';
+import { resCorrect, respError } from '../utils/funcResp';
 
 class MotoService {
   private motoODM:MotoODM;
@@ -19,6 +20,17 @@ class MotoService {
   public async create(moto:IMotorcycle) {
     const newMoto = await this.motoODM.create(moto);
     return resCorrect(201, this.createMotoDomain(newMoto));
+  }
+  public async findAll() {
+    const moto = await this.motoODM.findAll();
+    const motoDomain = moto.map((e:IMotorcycle) => this.createMotoDomain(e));
+    return resCorrect(200, motoDomain);
+  }
+  public async findById(id:string) {
+    if (!isValidObjectId(id)) return respError(422, 'Invalid mongo id');
+    const moto = await this.motoODM.findById(id);
+    if (!moto) return respError(404, 'Motorcycle not found');
+    return resCorrect(200, this.createMotoDomain(moto));
   }
 }
 export default MotoService;
