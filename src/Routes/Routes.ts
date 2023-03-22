40bd1fda6ffbb41a883e0969ec3e84bb7def6229
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 14:46:56 -0300

    feat:rota id car

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import CarsController from '../Controllers/CarsController';
-import CarODM from '../Models/CarODM';
 import CarsService from '../Services/CarsService';
 
 const routes = Router();
-const service = new CarsService(new CarODM());
+const service = new CarsService();
 const carsController = new CarsController(service);
 
 routes.post('/cars', (req, res, next) => carsController.create(req, res, next));
+routes.get('/cars', (req, res, next) => carsController.findAll(req, res, next));
+routes.get('/cars/:id', (req, res, next) => carsController.findById(req, res, next));
 
 export default routes;
