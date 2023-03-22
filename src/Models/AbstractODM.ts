d5424cc4f47c0fd07d73274401ae67ae979249e4
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 16:26:02 -0300

    FIX:ATUALIZANDO ABSTRACT DEIXANDO GENERICO

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -8,6 +8,30 @@ abstract class AbstractODM<T> {
     this.schema = schema;
     this.model = models[modelName] || model(modelName, this.schema);
   }
+  public async create(obj:T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+  public async findAll() {
+    return this.model.find();
+  }
+  public async findById(id:string) {
+    try {
+      const car = await this.model.findById(id);
+      return car;
+    } catch (e) {
+      return null;
+    }
+  }
+  public async findUpdate(id:string, car:Partial<T>) {
+    try {
+      const updateCar = await this.model.findByIdAndUpdate(id, car, {
+        new: true,
+      });
+      return updateCar;
+    } catch (error) {
+      return null;
+    }
+  }
 }
 
 export default AbstractODM;
