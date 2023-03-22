5a433b25913938cdfdfbcc819ef2cbfd9ab7aa71
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 13:21:01 -0300

    feat:interface icar

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,11 @@
+interface ICars {
+  id?: string,
+  model: string,
+  year: number,
+  color: string,
+  status? : boolean,
+  buyValue: number,
+  doorsQty: number,
+  seatsQty: number,
+}
+export default ICars;
