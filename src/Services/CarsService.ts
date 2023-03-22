7abd12a0b67d588c748ca69e03c2f4df266737d5
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 14:47:43 -0300

    feat:funçoes find e findid

--- src/Services/CarsService.ts
+++ src/Services/CarsService.ts
@@ -1,13 +1,12 @@
+import { isValidObjectId } from 'mongoose';
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
-import { resCorrect } from '../utils/funcResp';
+import { resCorrect, respError } from '../utils/funcResp';
 
 class CarsService {
-  private carODM:CarODM;
-  constructor(carODM:CarODM) {
-    this.carODM = carODM;
-  }
+  private carODM:CarODM = new CarODM();
+
   private createCarDomain(car:ICar):Car | null {
     if (car) {
       return new Car(car);
@@ -18,6 +17,17 @@ class CarsService {
   public async create(car:ICar) {
     const newCar = await this.carODM.create(car);
     return resCorrect(201, this.createCarDomain(newCar));
+  }
+  public async findAll() {
+    const cars = await this.carODM.findAll();
+    const carsDomain = cars.map((e) => this.createCarDomain(e));
+    return resCorrect(200, carsDomain);
+  }
+  public async findById(id: string) {
+    if (!isValidObjectId(id)) return respError(422, 'Invalid mongo id');
+    const car = await this.carODM.findById(id);
+    if (!car) return respError(404, 'Car not found');
+    return resCorrect(200, this.createCarDomain(car));
   }
 }
 export default CarsService;
