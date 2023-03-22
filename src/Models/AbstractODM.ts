4c726ef8187250fed0951dfb201e99acd12ec967
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 14:48:05 -0300

    fix:retirando os metodos

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -8,8 +8,6 @@ abstract class AbstractODM<T> {
     this.schema = schema;
     this.model = models[modelName] || model(modelName, this.schema);
   }
-  public async create(obj:T): Promise<T> {
-    return this.model.create({ ...obj });
-  }
 }
+
 export default AbstractODM;
