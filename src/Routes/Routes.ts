e0ff5d0309491fddb80910b5b7cbb81e8f95d8d8
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 16:26:12 -0300

    feat:routes update

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -9,6 +9,7 @@ const carsController = new CarsController(service);
 
 routes.post('/cars', (req, res, next) => carsController.create(req, res, next));
 routes.get('/cars', (req, res, next) => carsController.findAll(req, res, next));
+routes.put('/cars/:id', (req, res, next) => carsController.findUpdate(req, res, next));
 routes.get('/cars/:id', (req, res, next) => carsController.findById(req, res, next));
 
 export default routes;
