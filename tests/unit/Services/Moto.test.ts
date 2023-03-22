5b0c4e03581aa90929ad7b250a389cb354906182
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 18:46:37 -0300

    feat:teste de find e find id e tdd de find and update

--- tests/unit/Services/Moto.test.ts
+++ tests/unit/Services/Moto.test.ts
@@ -33,11 +33,50 @@ describe('Test de Moto Service', function () {
     new MotoODM(),
   );
 
-  it('Create motorcycle', async function () {
+  it('Criando moto', async function () {
     Sinon.stub(Model, 'create').resolves(motorycles[0]);
   
     const result = await service.create(motoInput);
 
     expect(result.message).to.deep.equal(motorycles[0]);
   });
+  it('buscando todas as motos', async function () {
+    Sinon.stub(Model, 'find').resolves(motorycles);
+
+    const result = await service.findAll();
+
+    expect(result.message).to.deep.equal(motorycles);
+  });
+
+  it('id invalido ao tetar buscar moto', async function () {
+    Sinon.stub(Model, 'findById').resolves(motorycles[0]);
+
+    const result = await service.findById('1');
+
+    expect(result.message).to.deep.equal({ message: 'Invalid mongo id' });
+  });
+  it('buscando moto por  id', async function () {
+    Sinon.stub(Model, 'findById').resolves(motorycles[0]);
+
+    const result = await service.findById('641b5b65100a8a05a3875790');
+
+    expect(result.message).to.deep.equal(motorycles[0]);
+  });
+
+  it('erro id ao tentar atualizar moto', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(motorycles[0]);
+
+    const result = await service.findUpdate('1', motoInput);
+
+    expect(result.message).to.deep.equal({ message: 'Invalid mongo id' });
+  });
+  it('atualizando moto ', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(motorycles[0]);
+
+    const result = await service.findUpdate('641b5b65100a8a05a3875790', motoInput);
+
+    expect(result.message).to.deep.equal(motorycles[0]);
+  });
+
+  afterEach(Sinon.restore);
 });
