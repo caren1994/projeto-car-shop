92c8697ff3bf077b348960f61cce3f46f1a0a0e6
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 13:45:41 -0300

    fix:importação

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import routes from './Routes/Routes';
-import ErrorHandler from './Middlewares/ErrorHandler';
+import ErrorHandler from './utils/Middlewares/ErrorHandler';
 
 const app = express();
 app.use(express.json());
