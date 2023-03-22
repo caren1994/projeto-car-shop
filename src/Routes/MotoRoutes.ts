31699b1630d9ef567c3577cc134c34339a3ebf91
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 18:56:31 -0300

    feat:routes put

--- src/Routes/MotoRoutes.ts
+++ src/Routes/MotoRoutes.ts
@@ -7,6 +7,7 @@ const service = new MotoService(new MotoODM());
 const motoController = new MotoController(service);
 const Mroutes = Router();
 Mroutes.post('/motorcycles', (req, res, next) => motoController.create(req, res, next));
+Mroutes.put('/motorcycles/:id', (req, res, next) => motoController.findUpdate(req, res, next));
 Mroutes.get('/motorcycles', (req, res, next) => motoController.findAll(req, res, next));
 Mroutes.get('/motorcycles/:id', (req, res, next) => motoController.findById(req, res, next));
 
