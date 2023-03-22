dcab9d4a46cc1aa27fd23c56793edd460c98daa0
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 16:28:38 -0300

    feat:update car

--- src/Controllers/CarsController.ts
+++ src/Controllers/CarsController.ts
@@ -41,5 +41,23 @@ class CarsController {
       next(error);
     }
   }
+  public async findUpdate(req:Request, res:Response, next:NextFunction) {
+    try {
+      const car: ICar = {
+        model: req.body.model,
+        year: req.body.year,
+        color: req.body.color,
+        status: req.body.status,
+        buyValue: req.body.buyValue,
+        doorsQty: req.body.doorsQty,
+        seatsQty: req.body.seatsQty,
+      };
+      const { id } = req.params;
+      const { status, message } = await this.service.findUpdate(id, car);
+      return res.status(status).json(message);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 export default CarsController;
