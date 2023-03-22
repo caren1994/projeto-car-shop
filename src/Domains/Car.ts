721e46684f3a73754e9b78f2b763a4c222265430
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 18:13:44 -0300

    fix:refatoração classe domain extends de classe vehicle

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,74 +1,27 @@
+import Vehicle from './Vehicle';
 import ICar from '../Interfaces/ICar';
 
-class Car {
-  protected id?: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean | undefined;
-  protected buyValue: number;
-  private doorsQty: number;
-  private seatsQty: number;
-  
-  constructor(
-    car:ICar,
-  ) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status;
-    this.buyValue = car.buyValue;
+class Car extends Vehicle {
+  private doorsQty: number | undefined;
+  private seatsQty: number | undefined;
+
+  constructor(car:ICar) {
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
   }
-  public getId(): string | undefined {
-    return this.id;
-  }
-  public setId(value: string | undefined) {
-    this.id = value;
-  }
-  public getModel(): string {
-    return this.model;
-  }
-  public setModel(value: string) {
-    this.model = value;
-  }
-  public getYear(): number {
-    return this.year;
-  }
-  public setYear(value: number) {
-    this.year = value;
-  }
-  public getColor(): string {
-    return this.color;
-  }
-  public setColor(value: string) {
-    this.color = value;
-  }
-  public getStatus(): boolean | undefined {
-    return this.status;
-  }
-  public setStatus(value: boolean | undefined) {
-    this.status = value;
-  }
-  public getBuyValue(): number {
-    return this.buyValue;
-  }
-  public setBuyValue(value: number) {
-    this.buyValue = value;
-  }
-  public getDoorsQty(): number {
-    return this.doorsQty;
-  }
-  public setDoorsQty(value: number) {
-    this.doorsQty = value;
-  }
-  public getSeatsQty(): number {
+  protected getSeatsQty(): number | undefined {
     return this.seatsQty;
   }
-  public setSeatsQty(value: number) {
+  protected setSeatsQty(value: number | undefined) {
     this.seatsQty = value;
+  }
+  
+  protected getDoorsQty(): number | undefined {
+    return this.doorsQty;
+  }
+  protected setDoorsQty(value: number | undefined) {
+    this.doorsQty = value;
   }
 }
 export default Car;
