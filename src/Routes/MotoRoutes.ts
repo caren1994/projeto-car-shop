6bda4b8ce9b1fa1fc028f94dc366ab117cb2fd62
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 18:40:40 -0300

    feat:rotas get

--- src/Routes/MotoRoutes.ts
+++ src/Routes/MotoRoutes.ts
@@ -7,5 +7,7 @@ const service = new MotoService(new MotoODM());
 const motoController = new MotoController(service);
 const Mroutes = Router();
 Mroutes.post('/motorcycles', (req, res, next) => motoController.create(req, res, next));
+Mroutes.get('/motorcycles', (req, res, next) => motoController.findAll(req, res, next));
+Mroutes.get('/motorcycles/:id', (req, res, next) => motoController.findById(req, res, next));
 
 export default Mroutes;
