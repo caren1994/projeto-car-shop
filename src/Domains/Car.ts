11a0ee760d9a57da0f95fedb6f5612d1fbff8351
caren <carenoliveirasilvapontes@gmail.com>
2023-03-22 13:21:41 -0300

    feat:domain car

---
+++ src/Domains/Car.ts
@@ -0,0 +1,74 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  protected id?: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean | undefined;
+  protected buyValue: number;
+  private _doorsQty: number;
+  private _seatsQty: number;
+  
+  constructor(
+    car:ICar,
+  ) {
+    this.id = car.id;
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status;
+    this.buyValue = car.buyValue;
+    this._doorsQty = car.doorsQty;
+    this._seatsQty = car.seatsQty;
+  }
+  public getId(): string | undefined {
+    return this.id;
+  }
+  public setId(value: string | undefined) {
+    this.id = value;
+  }
+  public getModel(): string {
+    return this.model;
+  }
+  public setModel(value: string) {
+    this.model = value;
+  }
+  public getYear(): number {
+    return this.year;
+  }
+  public setYear(value: number) {
+    this.year = value;
+  }
+  public getColor(): string {
+    return this.color;
+  }
+  public setColor(value: string) {
+    this.color = value;
+  }
+  public getStatus(): boolean | undefined {
+    return this.status;
+  }
+  public setStatus(value: boolean | undefined) {
+    this.status = value;
+  }
+  public getBuyValue(): number {
+    return this.buyValue;
+  }
+  public setBuyValue(value: number) {
+    this.buyValue = value;
+  }
+  public getDoorsQty(): number {
+    return this._doorsQty;
+  }
+  public setDoorsQty(value: number) {
+    this._doorsQty = value;
+  }
+  public getSeatsQty(): number {
+    return this._seatsQty;
+  }
+  public setSeatsQty(value: number) {
+    this._seatsQty = value;
+  }
+}
+export default Car;
